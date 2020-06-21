9be50253a6b237ab8ee968914749b4482eab70e6
Artyom <artglz63@gmail.com>
2020-06-21 22:28:26 +0300

    changed SportController.cs

--- Areas/Api/Controllers/SportController.cs
+++ Areas/Api/Controllers/SportController.cs
@@ -1,31 +1,48 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ExtremeInsiders.Data;
 using ExtremeInsiders.Entities;
+using ExtremeInsiders.Helpers;
 using ExtremeInsiders.Services;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 
 namespace ExtremeInsiders.Areas.Api.Controllers
 {
-  [Authorize]
-  [ApiController]
-  [Route("api/[controller]/[action]")]
-  public class SportController : Controller
-  {
-    private readonly UserService _userService;
-    private readonly ApplicationContext _db;
-
-    public SportController(UserService userService, ApplicationContext db)
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SportController : ControllerBase
     {
-      _userService = userService;
-      _db = db;
-    }
+        private readonly ApplicationContext _db;
+        private readonly UserService _userService;
 
-    [HttpGet]
-    public ActionResult<Sport> Get()
-    {
-      return _db.Sports.First().OfCulture(_userService.Culture);
+        public SportController(ApplicationContext db, UserService userService)
+        {
+            _db = db;
+            _userService = userService;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sport>>> GetSports()
+        {
+            return (await _db.Sports.ToListAsync()).OfCulture(_userService.Culture);
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sport>> GetSport(int id)
+        {
+            var sport = (await _db.Sports.FindAsync(id))?.OfCulture(_userService.Culture);
+
+            if (sport == null)
+            {
+                return NotFound();
+            }
+
+            return sport;
+        }
     }
-  }
 }
