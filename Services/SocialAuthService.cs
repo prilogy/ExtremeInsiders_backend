7e2595bfa8050be0b7d643621f887e9cf2ebb5df
ksydex <artglz63@gmail.com>
2020-08-06 00:01:16 +0300

    vk oauth fix

--- Services/SocialAuthService.cs
+++ Services/SocialAuthService.cs
@@ -140,25 +140,12 @@ namespace ExtremeInsiders.Services
 
     public override async Task<SocialAuthIdentity> GetIdentity(string code)
     {
-      var url =
-        $"https://oauth.vk.com/access_token?client_id={_appSettings.VkClientId}&client_secret={_appSettings.VkSecret}&redirect_uri={_appSettings.VkRedirect}&code={code}";
-
       try
       {
         using var client = new HttpClient();
+        
+        var url = $"https://api.vk.com/method/users.get?access_token={code}&v=5.110";
         var result = await client.GetAsync(url);
-        var identity = JsonConvert.DeserializeObject<VkIdentity>(await result.Content.ReadAsStringAsync());
-        var socialIdentity = new SocialAuthIdentity
-        {
-          Id = identity.user_id,
-          Email = identity.email ?? ""
-        };
-
-        if (socialIdentity.Email == "")
-          return null;
-
-        url = $"https://api.vk.com/method/users.get?user_ids={socialIdentity.Id}&access_token={identity.access_token}&v=5.110";
-        result = await client.GetAsync(url);
         var str = await result.Content.ReadAsStringAsync();
         
         const string toDeleteFirst = "{\"response\":[";
@@ -167,8 +154,13 @@ namespace ExtremeInsiders.Services
         str = str.Remove(str.IndexOf("]}"), toDeleteLast.Length);
         
         var profile = JsonConvert.DeserializeObject<VkIdentity>(str);
-        
-        socialIdentity.Name = profile.first_name + " " + profile.last_name;
+        var socialIdentity = new SocialAuthIdentity
+        {
+          Id = profile.id,
+          Email = profile.email,
+          Name = profile.first_name + " " + profile.last_name
+        };
+
         return socialIdentity;
       }
       catch
@@ -181,7 +173,7 @@ namespace ExtremeInsiders.Services
     {
       public string access_token { get; set; }
       public string expires_in { get; set; }
-      public string user_id { get; set; }
+      public string id { get; set; }
       public string email { get; set; }
       public string first_name { get; set; }
       public string last_name { get; set; }
