5789a1eecc7e8403a25bfa9ef4bf846a0099b8fb
ksydex <artglz63@gmail.com>
2020-08-17 12:43:05 +0300

    some new properties for sport model

--- Entities/Sport.cs
+++ Entities/Sport.cs
@@ -8,11 +8,14 @@ namespace ExtremeInsiders.Entities
 {
   public class Sport : EntityBase, ITranslatableEntity<Sport, SportTranslation>
   {
-    
-    [JsonIgnore]
-    public virtual List<Playlist> Playlists { get; set; }
-    [JsonIgnore]
-    public virtual List<Movie> Movies { get; set; }
+    [JsonIgnore] public virtual List<Playlist> Playlists { get; set; }
+    [JsonIgnore] public virtual List<Movie> Movies { get; set; }
+
+    [NotMapped] public int? BestPlaylistId => Playlists.OrderByDescending(x => x.LikesAmount).FirstOrDefault()?.Id;
+
+    [NotMapped]
+    public int? BestVideoId => Playlists.Select(x => x.Videos.OrderByDescending(v => v.LikesAmount).First())
+      .OrderByDescending(x => x.LikesAmount).FirstOrDefault()?.Id;
 
     public List<int> PlaylistsIds => Playlists?.Count > 0 ? Playlists.Select(x => x.Id).ToList() : null;
     public List<int> MoviesIds => Movies?.Count > 0 ? Movies.Select(x => x.Id).ToList() : null;
@@ -20,17 +23,17 @@ namespace ExtremeInsiders.Entities
     [JsonIgnore]
     [ForeignKey("BaseEntityId")]
     public virtual List<SportTranslation> Translations { get; set; }
-    [NotMapped]
-    public SportTranslation Content { get; set; }
+
+    [NotMapped] public SportTranslation Content { get; set; }
   }
 
-  public class SportTranslation : TranslatableEntityTranslation<Sport>, IDefaultTranslatableContent, ITranslationWithImage
+  public class SportTranslation : TranslatableEntityTranslation<Sport>, IDefaultTranslatableContent,
+    ITranslationWithImage
   {
     public string Name { get; set; }
     public string Description { get; set; }
-    
-    [JsonIgnore]
-    public int? ImageId { get; set; }
+
+    [JsonIgnore] public int? ImageId { get; set; }
     public virtual Image Image { get; set; }
   }
 }
