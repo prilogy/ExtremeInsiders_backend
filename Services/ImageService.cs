e6fb7fce4551cdb7d6289da6e1f51d72879631ac
ksydex <artglz63@gmail.com>
2020-09-07 18:51:47 +0300

    added magick comperssion

--- Services/ImageService.cs
+++ Services/ImageService.cs
@@ -6,6 +6,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ExtremeInsiders.Data;
 using ExtremeInsiders.Entities;
+using ImageMagick;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 
@@ -33,26 +34,40 @@ namespace ExtremeInsiders.Services
           8,
           10,
           HashAlgorithmName.SHA512);
-        
+
         var name = Regex.Replace(Convert.ToBase64String(algorithm.GetBytes(5)) + file.FileName, @"(\s|\\.|\/|\\)+", "");
-        
+
         var path = "/static/" + name;
-        await using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+        var systemPath = _appEnvironment.WebRootPath + path;
+
+        await using (var fileStream = new FileStream(systemPath, FileMode.Create))
         {
           await file.CopyToAsync(fileStream);
+          fileStream.Position = 0;
+          var optimizer = new ImageOptimizer
+          {
+            OptimalCompression = true,
+            IgnoreUnsupportedFormats = true
+          };
+          
+          optimizer.Compress(fileStream);
         }
 
+        var magickImage = new MagickImage(systemPath) {Quality = 40};
+        magickImage.Strip();
+        magickImage.Write(systemPath);
+
         var image = new Image {Path = path};
-        _db.Images.Add(image);
+        await _db.Images.AddAsync(image);
         await _db.SaveChangesAsync();
-        
+
         return image;
       }
-      catch 
+      catch (Exception ex)
       {
+        Console.WriteLine(ex);
         return null;
       }
-
     }
   }
 }
