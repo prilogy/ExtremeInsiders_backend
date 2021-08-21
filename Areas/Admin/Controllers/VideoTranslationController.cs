5a6978c04ee52461ad77325e69cad040598c2ec3
Artyom <artglz63@gmail.com>
2021-08-21 23:11:19 +0300

    append

--- Areas/Admin/Controllers/VideoTranslationController.cs
+++ Areas/Admin/Controllers/VideoTranslationController.cs
@@ -2,6 +2,8 @@
 using ExtremeInsiders.Areas.Admin.Models;
 using ExtremeInsiders.Data;
 using ExtremeInsiders.Entities;
+using ExtremeInsiders.Helpers;
+using ExtremeInsiders.Models;
 using ExtremeInsiders.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,10 +13,14 @@ namespace ExtremeInsiders.Areas.Admin.Controllers
     public class VideoTranslationController : ControllerTranslationBase<Video, VideoTranslation>
     {
         private readonly ApplicationContext _dbContext;
-        public VideoTranslationController(ApplicationContext context, ImageService imageService, ApplicationContext dbContext) : base(context,
+        private readonly FcmService _fcmService;
+
+        public VideoTranslationController(ApplicationContext context, ImageService imageService,
+            ApplicationContext dbContext, FcmService fcmService) : base(context,
             imageService)
         {
             _dbContext = dbContext;
+            _fcmService = fcmService;
         }
 
         public override IActionResult RedirectToBaseEntity(int id) =>
@@ -25,12 +31,13 @@ namespace ExtremeInsiders.Areas.Admin.Controllers
             var r = await base.Create(translation, imageSrc);
 
             if (translation.Id == 0) return r;
-            
+
             var video = await _dbContext.Videos.FindAsync(translation.BaseEntityId);
             if (video == null) return r;
-            
-            
-            
+
+            var n = DefaultNotification.NewVideo(video.OfCulture(translation.Culture));
+            await _fcmService.SendNotificationAsync(n, translation.CultureId);
+
             return r;
         }
     }
